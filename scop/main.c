e4136686a049534d84c4841733ad50fe32d66ef9
Vladyslav Popovych <mailuatc@gmail.com>
2018-01-27 15:09:35 +0200

    Initial Commit

---
+++ scop/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  scop
+//
+//  Created by Vladyslav Popovych on 1/27/18.
+//  Copyright © 2018 pet. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
